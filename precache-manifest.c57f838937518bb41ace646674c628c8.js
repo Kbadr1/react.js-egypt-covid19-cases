fb333fa9abfa3bcfddfc85e8f1fd0ccd3ffe8f4b
Kbadr1 <kbadr.dev@gmail.com>
2020-07-16 11:47:53 +0200

    Updates

---
+++ precache-manifest.c57f838937518bb41ace646674c628c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12254ebf2b9d4bb8016bc3387d80e6f",
+    "url": "/react.js-Egypt-covid19-cases/index.html"
+  },
+  {
+    "revision": "1d4aef18bd2966823564",
+    "url": "/react.js-Egypt-covid19-cases/static/css/main.1d7daac4.chunk.css"
+  },
+  {
+    "revision": "7cbd2a47ea88a548f1ea",
+    "url": "/react.js-Egypt-covid19-cases/static/js/2.a369a65b.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react.js-Egypt-covid19-cases/static/js/2.a369a65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4aef18bd2966823564",
+    "url": "/react.js-Egypt-covid19-cases/static/js/main.c9264222.chunk.js"
+  },
+  {
+    "revision": "6a400cd494b9a7865d3f",
+    "url": "/react.js-Egypt-covid19-cases/static/js/runtime-main.3aa3dd9e.js"
+  },
+  {
+    "revision": "218f2152d5fecb70178ee263c42888aa",
+    "url": "/react.js-Egypt-covid19-cases/static/media/flag.218f2152.jpg"
+  }
+]);
